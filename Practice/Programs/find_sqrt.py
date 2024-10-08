250326316b15513371ec87de3d7e5c6b2d98bfa6
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-08 23:10:23 +0530

    practice code1- find sqareroot using diiferent ways

---
+++ Practice/Programs/find_sqrt.py
@@ -0,0 +1,22 @@
+'''find the square root
+way 1 : exponential operartor
+
+'''
+num = 4;
+print(f"Square root of {num} is : ", num**(0.5));
+
+
+'''find the square root
+way 2 : using built-in function
+
+'''
+
+import math;
+print(f"Square root of {num} is : ", math.sqrt(num));
+
+'''
+1. Exponential Operator: Use the ** operator to raise a number to the power of 0.5. For example, num**(0.5) will give you the square root of num.
+
+2. Using Built-in Function: Import the math module and use the sqrt() function to find the square root of a number. For example, math.sqrt(num) will give you the square root of num.
+
+'''
