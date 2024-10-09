5306e9fa02c53a1ebfe985f59ad6bffbc10f2782
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-09 21:58:35 +0530

    new conversion code

--- Practice/Programs/kilometer_to_miles.py
+++ Practice/Programs/kilometer_to_miles.py
@@ -4,11 +4,23 @@
 '''
 text = "Converting kilometer to miles"
 print(text.center(50, "-"))
-km = float(input(" Enter the km for conversion : "))
-
 
 def km_to_miles(km):
-    return km * 0.621371
+            km_rs = km * 0.621371
+            print(f"{km} km to {km_rs:.2f} miles".center(50, ":"))
 
-miles = km_to_miles(km);
-print(f"{km} km to {miles:.2f} miles".center(50, ":"))
+while True:
+    try:
+        km = float(input("Enter km (0 for exiting) : "))
+        if km == 0:
+            break;
+        km_to_miles(km)
+    except ValueError:
+          print("Invalid input. Please enter a numeric value.")
+          
+          
+
+
+
+
+
