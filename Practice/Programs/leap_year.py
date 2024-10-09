971366d7864c4f243bb2b3c72db936c9808a234d
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-09 22:32:01 +0530

    Add leap year program wq

---
+++ Practice/Programs/leap_year.py
@@ -0,0 +1,12 @@
+
+# Python Program to Check leap Year or not
+def leap_year(year):
+    if year % 4 == 0:
+        return f"{year} is leap year." 
+    if year % 100 ==0 and  year % 400 == 0:
+        return f"{year} is leap year."
+    else:
+        return f"{year} is not a leap year."
+    
+year = int(input(" Enter Year : "))
+print(leap_year(year))
