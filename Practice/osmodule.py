af24ec4e2abe5645dbf4b1353bde1aed404703f7
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-13 14:36:07 +0530

    os module

---
+++ Practice/osmodule.py
@@ -0,0 +1,9 @@
+import os
+
+# retrieve current working directory
+print(os.getcwd())
+# retrieve list of file in current working directory
+print(os.listdir())
+
+# remdir() remove directory : but only empty files
+# rmtree() remove non-empty directory 
