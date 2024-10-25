ff05c7bd7d78d5fd4bd0ec2e9df4bee2cf7352a7
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-25 23:27:02 +0530

    update

--- Practice/protectedscope.py
+++ Practice/protectedscope.py
@@ -2,22 +2,49 @@
 class computer:
 
     def __init__(self):
-        self._maxPrice = 900
+        self.__maxPrice = 900
+        self.__discount = 10
     
     def sell(self):
-        print("Selling Price : {}".format(self._maxPrice))
+        print("Selling Price : {}".format(self.__maxPrice))
+    
+    def discount(self):
+        print("Discount Price : {}".format(self.__discount))
 
     def setMaxPrice(self,price):
         self._maxPrice = price    
-        
+
+     
+
+
+
+class computer:
+
+    def __init__(self):
+        self.__maxPrice = 900
+        self.__discount = 10
+    
+    def sell(self):
+        print("Selling Price : {}".format(self.__maxPrice))
+    
+    def discount(self):
+        print("Discount Price : {}".format(self.__discount))
+
+    def setMaxPrice(self,price):
+        self.__maxPrice = price    
+
+     
 
 
 
 c = computer();
 c.sell(); #900
 
-c.setMaxPrice(3000);
-c.sell(); #3000
+#change the price
+c.__maxPrice = 4000;
+c.sell(); 
 
-c.setMaxPrice(4000);
-c.sell(); #4000
+
+c.setMaxPrice(6000);
+c.sell();
+c.discount(); 
