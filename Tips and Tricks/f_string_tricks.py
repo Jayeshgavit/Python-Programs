24c94b42594b204c107d3b47c05bb2e713ed92c5
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-31 22:14:57 +0530

    code realted to datetime f string added

--- Tips and Tricks/f_string_tricks.py
+++ Tips and Tricks/f_string_tricks.py
@@ -10,7 +10,13 @@ print(f"Price (.2f): {price:,.2f}")
 
 
 #Increase Number Readability
-
 number: int = 1000000
 print(f"Number: {number:_}")
 print(f"Number: {number:,}")
+
+#dealing with date and time
+
+from datetime import datetime 
+
+now: datetime = datetime.now()
+print(f"Now: {now:%d-%m-%y (%H:%M:%S)}")
