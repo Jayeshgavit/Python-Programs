ec23d025d60c98ed7099f995a0358a14383ec39f
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-31 22:07:14 +0530

    code - Increase Number Readability

--- Tips and Tricks/f_string_tricks.py
+++ Tips and Tricks/f_string_tricks.py
@@ -5,3 +5,12 @@ print("Price : ", round(price,2))
 
 # Using f-string
 print(f"Price (.2f): {price:.2f}")
+#increase readability
+print(f"Price (.2f): {price:,.2f}")
+
+
+#Increase Number Readability
+
+number: int = 1000000
+print(f"Number: {number:_}")
+print(f"Number: {number:,}")
