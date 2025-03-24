e8f5f7aadc15f03d80f17483170ac329a151b387
Jayeshgavit <gavitjayesh777@gmail.com>
2025-03-24 20:35:45 +0530

    Practice code

---
+++ Practice/Programs/squareroot.py
@@ -0,0 +1,13 @@
+# Solution - 1 
+num = 49
+result = num ** (0.5)  #(1/2)
+print("The square of", num, "is", result)
+
+
+
+# Solution - 2 
+import math
+
+num2 = int(input("Enter the Number here : "))
+resul2 = math.sqrt(num2)
+print("The square root of", num2, "is", resul2)
